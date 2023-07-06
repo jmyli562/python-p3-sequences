342eea96ccec8b3be1a8188463058b1e8a1bd6c9
Jimmy Li <jmyli562@gmail.com>
2023-07-06 11:31:12 -0700

    finished lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
+
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print("[]")
+        return
+    sequence = []
+    a, b = 0, 1
+
+    while len(sequence) < length:
+        sequence.append(a)
+        a, b = b, a + b
+
+    print(sequence)
